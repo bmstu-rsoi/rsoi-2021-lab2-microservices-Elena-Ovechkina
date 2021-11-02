3644d5a3bf678d45b19c03ac730f2a8c29dfe075
Elena-Ovechkina <70863711+Elena-Ovechkina@users.noreply.github.com>
2021-11-02 22:30:38 +0300

    Описала типовой документ авто в БД

---
+++ car/app/models/car.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CarSchema = new Schema({
+  car_uid: { type: String, required: true },
+  brand: { type: String, required: true },
+  model: { type: String, required: true },
+  registration_number: { type: String, required: true },
+  power: Number,
+  price: { type: Number, required: true },
+  type: { type: String, enum: ['SEDAN', 'SUV', 'MINIVAN', 'ROADSTER'] },
+  availability: { type: Boolean, required: true, default: true }
+});
+
+
+mongoose.model('Car', CarSchema);
+
