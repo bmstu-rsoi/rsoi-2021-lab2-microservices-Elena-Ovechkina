c9e258d3cd2bcbacf0665288097a33c9c30c2629
Elena-Ovechkina <70863711+Elena-Ovechkina@users.noreply.github.com>
2021-11-04 01:43:43 +0300

    Типовой документ Payment в БД

---
+++ payment/app/models/payment.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PaymentSchema = new Schema({
+  payment_uid: { type: String, required: true },
+  status: { type: String, required: true, enum: ['PAID', 'CANCELED'] },
+  price: { type: Number, required: true }
+});
+
+
+mongoose.model('Payment', PaymentSchema);
