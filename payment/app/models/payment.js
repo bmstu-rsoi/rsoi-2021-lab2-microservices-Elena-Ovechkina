5a7cb28b2a835fe7a07b0498a861240cbda283e1
Elena-Ovechkina <70863711+Elena-Ovechkina@users.noreply.github.com>
2021-11-20 02:15:18 +0300

    Update payment.js
    Типовой документ Payment в БД

--- payment/app/models/payment.js
+++ payment/app/models/payment.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const PaymentSchema = new Schema({
   payment_uid: { type: String, required: true },
-  status: { type: String, required: true, enum: ['PAID', 'CANCELED'] },
+  status: { type: String, required: true, enum: ['PAID', 'CANCELED'], default: 'PAID'},
   price: { type: Number, required: true }
 });
 
